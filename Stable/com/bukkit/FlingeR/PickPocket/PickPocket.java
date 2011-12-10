5783b32acca8c4fdc794d124288a8ca1e837719e
Xavi Ablaza <xlablaza@gmail.com>
2011-12-10 17:17:08 +0800

    removed deprecated methods

--- Stable/com/bukkit/FlingeR/PickPocket/PickPocket.java
+++ Stable/com/bukkit/FlingeR/PickPocket/PickPocket.java
@@ -1,117 +1,62 @@
 package com.bukkit.FlingeR.PickPocket;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.HashMap;
+import java.util.logging.Logger;
 
 import org.bukkit.Material;
-import org.bukkit.Server;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
-import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-import com.nijiko.permissions.PermissionHandler;
-import com.nijikokun.bukkit.Permissions.Permissions;
 
 public class PickPocket extends JavaPlugin {
-	    private static Server Server = null;
-	    public static HashMap<String, String> pconfig = new HashMap<String, String>();
+		public static PickPocket plugin;
+		
+		public final Logger logger = Logger.getLogger("Minecraft");
 	    public static HashMap<Material, Double> IList = new HashMap<Material, Double>();
-	    public static PermissionHandler Permissions;
-	    private final PickPocketEntityListener entityListener = new PickPocketEntityListener(this);
+	    private final PickPocketPlayerListener playerListener = new PickPocketPlayerListener(this);
+	    
+	    static int timmhartel = 325;
+	    static int and = 1;
+	    static boolean alannahsymes = true;
+	    String aredating = "[PickPocket] Attempt successful.";
+	    String omg_seriously = "[PickPocket] %n tried to pickpocket you!";
+	    String whatsupwithlife = "[PickPocket] Your hand slipped.";
 	    
 	    public void onEnable() {
-	    	
-	    File yml = new File(getDataFolder()+"/config.yml");
-	    
-	    if (!yml.exists()) {
-	        new File(getDataFolder().toString()).mkdir();
-	    try {
-	    yml.createNewFile();
-	    }
-	    catch (IOException ex) {
-	    System.out.println("[PickPocket] cannot create file "+yml.getPath());
-	    }
-	       }  
-	    
-		pconfig.put("PickPocketTool", getConfiguration().getString("PickPocketTool", "325"));
-		pconfig.put("DamageOnFail", getConfiguration().getString("DamageOnFail", "1"));
-		pconfig.put("LooseToolOnFail", getConfiguration().getString("LooseToolOnFail", "NO"));
-		pconfig.put("FailToSteal", getConfiguration().getString("FailToSteal", "[PickPocket] You have Fail!!"));
-		pconfig.put("ItemStolen", getConfiguration().getString("ItemStolen", "[PickPocket] You have stolen an item!!"));
-		pconfig.put("TryToStealYou", getConfiguration().getString("TryToStealYou", "[PickPocket] %n has tried to steal you!!"));
-		pconfig.put("HasStolenYou", getConfiguration().getString("HasStolenYou", "[PickPocket] %n has stolen you!!"));
-	     getConfiguration();
-	     Server = getServer();
+	    	try {
+	    		File fileconfig = new File(getDataFolder(), "config.yml");
+	    		if (!fileconfig.exists()) {
+	    			getDataFolder().mkdir();
+	    			new File(getDataFolder(), "config.yml");
+	    			this.getConfig().options().copyDefaults(true);
+	    			saveConfig();
+	    		}
+	    	} catch (Exception e1){
+	    		e1.printStackTrace();
+	    	}
+	    	sweet_kiwis();
 	     PluginManager pm = getServer().getPluginManager();
-	        if (!(new File(getDataFolder(), "config.yml")).exists()) {
-	         defaultConfig();
-	        }  
-	        loadConfig();
-	        pm.registerEvent(Event.Type.ENTITY_DAMAGE, entityListener, Event.Priority.Normal, this);
-	        setupPermissions();
-	        try {
-				readProperties();
-			} catch (Exception e) {
-				System.out.println("[PickPocket] Exception while reading items.properties.");
-			}
-	        System.out.println("[PickPocket] v" + getDescription().getVersion() + " by FlingeR sucesfuly enabled.");
+	     PluginDescriptionFile pdf = this.getDescription();
+	        pm.registerEvent(Event.Type.PLAYER_INTERACT_ENTITY, playerListener, Event.Priority.Normal, this);
+	        this.logger.info( "[PickPocket] version " + pdf.getVersion() + " by MeneXia is enabled!" );
+	    	this.logger.info("[PickPocket] Permissions will default to op if SuperPerms is not present.");
 	    }
 	    
 	    public void onDisable() {
-	        System.out.println("[PickPocket] Sucesfuly disabled");
-
+	    	saveConfig();
+	        this.logger.info("[PickPocket] disabled!");
 	    }
 	    
-	    private void loadConfig() {
+	    public void sweet_kiwis() {
+	    	timmhartel = this.getConfig().getInt("PickPocketTool");
+	    	and = this.getConfig().getInt("DamageOnFail");
+	    	alannahsymes = this.getConfig().getBoolean("LoseToolonFail");
+	    	aredating = this.getConfig().getString("ItemStolen");
+	    	omg_seriously = this.getConfig().getString("TryToStealYou");
+	    	whatsupwithlife = this.getConfig().getString("FailToSteal");
 	    }
-
-	    private void defaultConfig() {
-	    }
-	    
-	    private void setupPermissions() {
-	     Plugin permissions = this.getServer().getPluginManager().getPlugin("Permissions");
-
-	        if (Permissions == null) {
-	         if (permissions != null) {
-	             Permissions = ((Permissions)permissions).getHandler();
-	            } else {
-	             System.out.println("Permission system not detected, defaulting to OP");
-	            }
-	        }
-	    }
-
-	    public static boolean hasPermissions(Player p, String s) {
-	        if (Permissions != null) {
-	            return Permissions.has(p, s);
-	        } else {
-	            return p.isOp();
-	        }
-	    }
-	    
-	    public static Server getBukkitServer() {
-	        return Server;
-	    }
-
-		public static void readProperties() throws Exception {
-			String fileName = "plugins/PickPocket/items.properties";
-			BufferedReader reader = new BufferedReader(new FileReader(fileName));
-			String line;
-			while ((line = reader.readLine()) != null) {
-				if ((line.trim().length() == 0) || 
-						(line.charAt(0) == '#')) {
-					continue;
-				}
-				int keyPosition = line. indexOf('=');
-				Material item = Material.getMaterial(line.substring(0, keyPosition).trim());
-				double value = Double.parseDouble(line.substring(keyPosition+1, line.length()).trim());
-				IList.put(item, value);
-			}
-	        System.out.println("[PickPocket] Configuration sucesfuly loaded");			
-		}
 		
 		public static boolean canBeStolen(Material type){
 			return(IList.containsKey(type));
